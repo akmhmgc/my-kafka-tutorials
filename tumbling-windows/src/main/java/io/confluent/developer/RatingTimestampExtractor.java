d4d4a4fd461b5fb6e8ac77de1b4550d6d48801c9
akmhmgc <akimuhamaguchi@icloud.com>
2024-01-14 16:13:04 +0900

    time_extractor

---
+++ tumbling-windows/src/main/java/io/confluent/developer/RatingTimestampExtractor.java
@@ -0,0 +1,24 @@
+package io.confluent.developer;
+
+import io.confluent.developer.avro.Rating;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.streams.processor.TimestampExtractor;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class RatingTimestampExtractor implements TimestampExtractor {
+    @Override
+    public long extract(ConsumerRecord<Object, Object> record, long previousTimestamp) {
+        final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+
+        // record.value()がObject型なのでRating型にキャストしている
+        String eventTime = ((Rating)record.value()).getTimestamp();
+
+        try {
+            return sdf.parse(eventTime).getTime();
+        } catch(ParseException e) {
+            return 0;
+        }
+    }
+}
